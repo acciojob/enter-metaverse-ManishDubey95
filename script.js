a320434bdb140518adfb5bad73b71a4fb322a002
ManishDubey95 <136079775+ManishDubey95@users.noreply.github.com>
2023-10-30 20:47:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const button = document.querySelector("#enterBtn");
+const fun = () => {
+	const heading = document.createElement("h1");
+	heading.innerText = "Entered Metaverse";
+
+	const para = document.querySelector("#status");
+	
+	const body = document.querySelector("body");
+
+	body.insertBefore(heading, para);
+	para.remove();
+
+	console.log(body);
+	
+}
+
+button.addEventListener("click",fun);
